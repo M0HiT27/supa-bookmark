83ec4b8b8b8e2179094ae27018922e4cab2ff250
M0HiT27 <raghuwanshimohit1@gmail.com>
2026-02-18 22:20:47 +0530

    [Update] : Auth setup

---
+++ src/app/auth/route.ts
@@ -0,0 +1,17 @@
+import { createClient } from "../../lib/supabase/server";
+import { NextResponse } from 'next/server'
+
+export async function POST(request: Request) {
+  const supabase = await createClient()
+
+  // Check if a user is logged in before signing out
+  const { data: { user } } = await supabase.auth.getUser()
+
+  if (user) {
+    await supabase.auth.signOut()
+  }
+
+  return NextResponse.redirect(new URL('/', request.url), {
+    status: 302,
+  })
+}
